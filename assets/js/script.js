9df9059a2dea0fcf3b2c852fc1a0840618663c40
Kendra Staver <staverkendra@gmail.com>
2021-10-23 19:13:21 -0700

    added global variables and generatePass func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,61 @@
 // Assignment code here
+var generateBtn = document.querySelector("#generate");
 
+var lowerCase = confirm("Does your password need a lowercase?");
+var upperCase = confirm("Does your password need an uppercase?");
+var symbol = confirm("Does your password need a symbol?");
+var number= confirm("Does your password need a number?");
+var passwordLength = prompt("What is the minimum length of your password?");
+var passwordInt = pasreInt(passwordLength);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// If statement to determin the length of the password and check
+// to see if the password is at least 8 characters long
+if (passwordLength >= 8 && password <= 128){
+
+  // Function for generating passwords
+  function generatePassword(){
+    var emptyContainer = "";
+    var passLowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var passUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passSymbol = "!'#$%&()*+-./;:<>=?@[]{}~^";
+    var passNumber = "0123456789";
+
+    // Validate wether or not the user has said yes to the above mentioned
+    // prompts. If yes then add the appropritae varibles to the empty container
+
+    // Lower case
+    if(lowerCase == ture){
+      emptyContainer += passLowerCase;
+    };
+
+    // Upper case
+    if(upperCase == true){
+      emptyContainer += passUpperCase;
+    };
+
+    // Symbol
+    if(passSymbol == true){
+      emptyContainer += passSymbol;
+    };
+
+    // Number
+    if(number == true){
+      emptyContainer += passNumber;
+    };
+
+    // Create password
+    var password = "";
+    for( var i = 0; i < passwordInt; i++){
+      password += emptyContainer.charAt(Math.floor(Math.random()* emptyContainer.length));
+    };
+    return password;
+  };
+}else{
+   alert("Password lewngth must be between 8 and 128 characters.");
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +63,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
